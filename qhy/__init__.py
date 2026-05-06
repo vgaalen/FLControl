0c2eb1846870141ff12159a71e783fa51ee7042e
Remon van Gaalen <remon.vangaalen@inaf.it>
2026-05-06 16:18:23 +0200

    Update default QHY ROI

--- qhy/__init__.py
+++ qhy/__init__.py
@@ -185,7 +185,7 @@ class QhyCam:
         self.Shutters = {'N/A': 0}
         self.Modes = {'N/A': 0}
         self.roi = False
-        self.setRoi("4000,3400,2000,1500")
+        self.setRoi("3500,2750,2500,1500")
 
     def Start(self):
         pass
